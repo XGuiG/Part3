d3f91a709ffdead6b537d2c5dfe185ffd8aec7ae
XGuiG <1351739465@qq.com>
2023-03-05 16:23:24 +0800

    uibuild

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ morgan.token("extentionJSON", (request, response) =>
   JSON.stringify(request.body)
 );
 morgan.format(
-  "post",
+  "json",
   ":method :url :status :res[content-length] - :response-time ms :extentionJSON"
 );
-app.use(morgan("post"));
+app.use(morgan("json"));
 app.use(express.static("build"));
 
 app.get("/api/persons", (request, response) => {
@@ -28,40 +28,41 @@ app.get("/api/persons", (request, response) => {
 
 app.get("/info", (request, response) => {
   const date = new Date();
-  response.send(
-    `<div><p>Phonebook has info for ${persons.length} persons</p>${date}</div>`
-  );
+  Person.find({}).then((persons) => {
+    response.send(
+      `<div><p>Phonebook has info for ${persons.length} persons</p>${date}</div>`
+    );
+  });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((p) => p.id === id);
-
-  if (person) {
-    // response.json(person);
-    Person.findById(request.params.id).then((person) => {
-      response.json(person);
-    });
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((p) => p.id !== id);
-
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.use(express.json());
 // app.use(morgan.token());
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
   // console.log(request.body)
   if (!body.name || !body.number) {
     return response.status(400).json({
-      error: "name missing/number missing",
+      error: "name / number missing",
     });
   }
   //   else if (persons.find((p) => p.name === body.name)) {
@@ -76,14 +77,45 @@ app.post("/api/persons", (request, response) => {
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 
   //   persons = persons.concat(person);
   //   response.json(person);
   //   console.log(request.body)
 });
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const {name, number} = request.body;
+//   console.log(body);
+//   const person = {
+//     name: body.name,
+//     number: body.number,
+//   };
+
+  Person.findByIdAndUpdate(request.params.id, 
+    {name, number}, 
+    { new: true, runValidators: true, context: 'query' })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
+  }
+  next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
