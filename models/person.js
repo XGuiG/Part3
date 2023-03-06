7a55ea425cc66d17cef541a30b23bcd566312a75
XGuiG <1351739465@qq.com>
2023-03-06 16:18:13 +0800

    Revert "update"
    This reverts commit 09ad9fa44430bdda09eac05f0c3ea1daba6e1bd4.

--- models/person.js
+++ models/person.js
@@ -1,5 +1,20 @@
 const mongoose = require("mongoose");
 
+mongoose.set("strictQuery", false);
+
+const url = process.env.MONGODB_URI;
+
+console.log("connecting to", url);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -12,7 +27,7 @@ const personSchema = new mongoose.Schema({
     validate: {
       validator: function (v) {
         return /^\d{2,3}-\d{1,}$/.test(v);
-      },
+      }
     },
     required: true,
   },
